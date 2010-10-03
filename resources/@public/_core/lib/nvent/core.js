930f2b9581b10b5437aafda470445f581ad6cda0
Justin Deal <justin.deal@gmail.com>
2010-10-03 22:03:50 +0000

    more save/delete work

--- resources/@public/_core/lib/nvent/core.js
+++ resources/@public/_core/lib/nvent/core.js
@@ -1,88 +1,122 @@
 // core object changes; be careful, changes to Object have side effects!
 if (typeof(String.prototype.trim) === "undefined") {
-	String.prototype.trim = function() {
-		return this.replace(/^\s+|\s+$/g, '');
-	}
+    String.prototype.trim = function() {
+        return this.replace(/^\s+|\s+$/g, '');
+    };
 }
 if (typeof(String.prototype.startsWith) === "undefined") {
-	String.prototype.startsWith = function(s) {
-		s = "" + s;
-		if (s.length > this.length){
-			return false;
-		}
-		return this.substring(0,s.length) == s;
-	}
+    String.prototype.startsWith = function(s) {
+        s = "" + s;
+        if (s.length > this.length){
+            return false;
+        }
+        return this.substring(0,s.length) == s;
+    };
 }
 if (typeof(String.prototype.endsWith) === "undefined") {
-	String.prototype.endsWith = function(s) {
-		s = "" + s;
-		if (s.length > this.length){
-			return false;
-		}
-		return this.substring(this.length - s.length) == s;
-	}
+    String.prototype.endsWith = function(s) {
+        s = "" + s;
+        if (s.length > this.length){
+            return false;
+        }
+        return this.substring(this.length - s.length) == s;
+    };
 }
 
 // provide require function for the browser
+var require;
 if (typeof(require) === "undefined") {
-	var require = this["require"] = function require(name){
-		if (name in require.registry){
-			return require.registry[name]
-		}
-		return null;
-	}
-	require.registry = {}
-	require.registry["nvent/register"] = function(name){
-		require.registry[name] = {}
-		return require.registry[name]
-	}
-	if (typeof($)!=="undefined"){
-		require.registry["jquery"] = $
-	}
-	if (typeof(async)!=="undefined"){
-		require.registry["async"] = async
-	}
+    var require = this.require = function require(name){
+        if (name in require.registry){
+            return require.registry[name];
+        }
+        return null;
+    };
+    require.registry = {};
+    require.registry["nvent/register"] = function(name){
+        require.registry[name] = {};
+        return require.registry[name];
+    };
+    if (typeof($)!=="undefined"){
+        require.registry.jquery = $;
+    }
+    if (typeof(async)!=="undefined"){
+        require.registry.async = async;
+    }
 }
 
 (function(exports){
 
-	var isArray = exports.isArray = function isArray(o){
-		return Object.prototype.toString.call(o) === '[object Array]';
-	}
+    var isUndefined = exports.isUndefined = function(value){
+        if (typeof(value)==="undefined"){
+            return true;
+        }
+        return false;
+    };
 
-	var isString = exports.isString = function isString(o){
-		return Object.prototype.toString.call(o) === '[object String]';
-	}
+    var isNothing = exports.isNothing = function(value){
+        if (typeof(value)==="undefined" || value===null){
+            return true;
+        }
+        return false;
+    };
+    
+    var isBlank = exports.isBlank = function(value){
+        if (isNothing(value) || value===""){
+            return true;
+        }
+        return false;
+    };
 
-	var getObject = exports.getObject = function getObject(parts,obj,create){
+    var isArray = exports.isArray = function isArray(o){
+        return Object.prototype.toString.call(o) === '[object Array]';
+    };
 
-		if ( typeof parts === 'string' ) {
-			parts = parts.split('.');
-		}
+    var isString = exports.isString = function isString(o){
+        return Object.prototype.toString.call(o) === '[object String]';
+    };
+    
+    var error = exports.error = function error(type,message){
+        return {success:false,error:type,error_message:message};
+    };
+    
+    var success = exports.success = function success(result){
+        if (isUndefined(result)){
+            return {success:true};
+        } else {
+            return {success:true,result:result};
+        }
+    };
 
-		var p;
+    var getObject = exports.getObject = function getObject(parts,obj,create){
 
-		while (obj && parts.length ) {
-			p = parts.shift();
-			if ( obj[p] === undefined && create ) {
-				obj[p] = {};
-			}
+        if ( typeof parts === 'string' ) {
+            parts = parts.split('.');
+        }
 
-			obj = obj[p];
-		}
+        var p;
 
-		return obj;
-	}
+        while (obj && parts.length ) {
+            p = parts.shift();
+            if ( obj[p] === undefined && create ) {
+                obj[p] = {};
+            }
 
-	var array = exports.array = {}
+            obj = obj[p];
+        }
 
-	array.mapExpand = function arrayMapExpand(array,f){
-		array = array.map(f)
-		var expandedArray = []
-		for (var i = 0; i < array.length; i++){
-			expandedArray = expandedArray.concat(array[i])
-		}
-		return expandedArray;
-	}
+        return obj;
+    };
+
+    var array = exports.array = {};
+
+    array.mapExpand = function arrayMapExpand(array,f){
+        array = array.map(f);
+        var expandedArray = [];
+        for (var i = 0; i < array.length; i++){
+            expandedArray = expandedArray.concat(array[i]);
+        }
+        return expandedArray;
+    };
 
 })(typeof exports === 'undefined'? require("nvent/register")("nvent/core") : exports);
